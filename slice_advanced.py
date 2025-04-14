ed253397a0e42d28678e92dd85fdeaae9f36eb04
Tiago Farach <tnfarach@mail.austral.edu.ar>
2025-04-14 20:05:19 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,5 +1,11 @@
 def slice_advanced():
-    # Código a implementar utilizando input.
+   texto = input("hola como estas:")
+
+   resultado = texto[4::2]
+
+   print(resultado)
+   
+slice_advanced() # Código a implementar utilizando input.
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
