6ed28494fc1be35bf0bf6f5ee83df5bb80983778
Tiago Farach <tnfarach@mail.austral.edu.ar>
2025-04-14 20:09:10 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -14,8 +14,3 @@ def check_vowels():
     print(f"Contiene u: {contiene_u}")
 
 check_vowels()
-  # Código a implementar utilizando input.
-
-
-# Para verificar este ejercicio ejecutar el comando
-# `pytest tp3_in_string_test.py` o `python tp3_in_string_test.py`
