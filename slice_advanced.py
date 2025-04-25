6a5393c1f62828a92a4a52ca74147533e28671a2
Tiago Farach <tnfarach@mail.austral.edu.ar>
2025-04-25 16:16:22 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -5,7 +5,7 @@ def slice_advanced():
 
    print(resultado)
    
-slice_advanced() # Código a implementar utilizando input.
+ # Código a implementar utilizando input.
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
